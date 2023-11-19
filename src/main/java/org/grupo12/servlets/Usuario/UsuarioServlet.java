8c0f79e30b644d4639cbe1350903fad0a11c17e9
ElChicoAracnido10 <miguel.soria@unmsm.edu.pe>
2023-11-19 04:00:27 -0500

    Se conecto la informa la parte de usario
    yata todo , falta que se agregue lo de favortitos , adoptados, y apadrinados a la base de datos

--- src/main/java/org/grupo12/servlets/Usuario/UsuarioServlet.java
+++ src/main/java/org/grupo12/servlets/Usuario/UsuarioServlet.java
@@ -1,14 +1,14 @@
-package serverlet.Usuario;
+package org.grupo12.servlets.Usuario;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.io.IOException;
 
+import java.io.IOException;
 @WebServlet("/usuario")
-public class UsuarioServerlet extends HttpServlet {
+public class UsuarioServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/usuario/usuario.jsp").forward(request, response);
     }
