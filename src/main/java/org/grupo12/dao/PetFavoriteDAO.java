16269d6424bdb4dcf484a46005a7160b16440a4d
Oscar Pairazaman <oscar.pairazaman2@unmsm.edu.pe>
2023-11-25 18:56:05 -0500

    favoritos petlist

---
+++ src/main/java/org/grupo12/dao/PetFavoriteDAO.java
@@ -0,0 +1,109 @@
+package org.grupo12.dao;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PetFavoriteDAO {
+    private HikariDataSource dataSource;
+
+    public PetFavoriteDAO(HikariDataSource dataSource) {this.dataSource = dataSource;}
+
+    public boolean addFavorite(int userId, int petId) {
+        String sql = "INSERT INTO UserPet (UserId, PetId, IsFavorite)VALUES(?, ?, 1)";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, petId);
+
+            int rowsUpdated = statement.executeUpdate();
+            return rowsUpdated > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean removeFavorite(int userId, int petId) {
+        String sql = "UPDATE UserPet SET IsFavorite = 0 WHERE UserId = ? AND PetId = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, petId);
+
+            int rowsUpdated = statement.executeUpdate();
+            return rowsUpdated > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean updateFavorite(int userId, int petId, int isFavorite) {
+        String sql = "UPDATE UserPet SET IsFavorite = ? WHERE UserId = ? AND PetId = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, isFavorite);
+            statement.setInt(2, userId);
+            statement.setInt(3, petId);
+
+            int rowsUpdated = statement.executeUpdate();
+            return rowsUpdated > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public int isFavorite(int userId, int petId) {
+        String sql = "SELECT IsFavorite FROM UserPet WHERE UserId = ? AND PetId = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+            statement.setInt(2, petId);
+
+            int isFavorite = 2;
+            var result = statement.executeQuery();
+
+            if (result.next()) {
+                isFavorite = result.getInt("IsFavorite");
+            }
+            return isFavorite;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public List<Integer> getFavoritePetsByUser(int userId) {
+        String sql = "SELECT PetId FROM UserPet WHERE UserId = ? AND IsFavorite = 1";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+
+            var result = statement.executeQuery();
+            List<Integer> pets = new ArrayList<>();
+            while (result.next()) {
+                pets.add(result.getInt("PetId"));
+            }
+            return pets;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/org/grupo12/servlets/Pet/PetFavoriteServlet.java
@@ -0,0 +1,91 @@
+package org.grupo12.servlets.Pet;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.grupo12.dao.PetFavoriteDAO;
+import org.grupo12.models.User;
+import org.grupo12.services.implementation.PetFavoriteService;
+import org.grupo12.util.ConnectionDB;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/petfavorite")
+public class PetFavoriteServlet extends HttpServlet {
+    private final HikariDataSource dataSource = ConnectionDB.getDataSource();
+
+    private final PetFavoriteService petFavoriteService;
+
+    public PetFavoriteServlet() {
+        this.petFavoriteService = new PetFavoriteService(new PetFavoriteDAO(dataSource));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try{
+            User user = (User) request.getSession().getAttribute("user");
+            int userId = user.getUserId();
+            int petId = Integer.parseInt(request.getParameter("petId"));
+
+            List<Integer> isFavorite = petFavoriteService.getFavoritePetsByUser(userId);
+
+            if(isFavorite.contains(petId)){
+                response.getWriter().write("{\"success\": true, \"isFavorite\": true}");
+            }else{
+                response.getWriter().write("{\"success\": true, \"isFavorite\": false}");
+            }
+
+        }catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try{
+            User user = (User) request.getSession().getAttribute("user");
+            int userId = user.getUserId();
+            int petId = Integer.parseInt(request.getParameter("petId"));
+
+            int isFavorite = petFavoriteService.isFavorite(userId, petId);
+            boolean resp;
+
+            //Remover de favoritos
+            if(isFavorite == 1){
+                resp = petFavoriteService.updateFavorite(userId, petId, 0);
+                if(!resp){
+                    response.getWriter().write("{\"success\": false}");
+                    return;
+                }
+                response.getWriter().write("{\"success\": true, \"isFavorite\": false}");
+            }
+            //Actualizar favoritos
+            else if(isFavorite == 0){
+                resp = petFavoriteService.updateFavorite(userId, petId, 1);
+                if(!resp){
+                    response.getWriter().write("{\"success\": false}");
+                    return;
+                }
+                response.getWriter().write("{\"success\": true, \"isFavorite\": true}");
+
+            }
+            //Agregar a favoritos
+            else{
+                resp = petFavoriteService.addFavorite(userId, petId);
+                if(!resp){
+                    response.getWriter().write("{\"success\": false}");
+                    return;
+                }
+                response.getWriter().write("{\"success\": true, \"isFavorite\": true}");
+
+            }
+
+        }catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+}
